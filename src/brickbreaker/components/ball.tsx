070654fee8118ee71f38c2f6adc8efa92b8183fe
Kelvin <chuakid@gmail.com>
2024-08-11 23:23:07 +0800

    chore: remove console logs

--- src/brickbreaker/components/ball.tsx
+++ src/brickbreaker/components/ball.tsx
@@ -41,19 +41,13 @@ export class Ball {
 
     moveBall(): void {
         this.pos.x += this.speed.x;
-        this.pos.y += this.speed.y;
-        console.log(this.speed);
-        
+        this.pos.y += this.speed.y;       
     }
 
     setSpeed(speed: number) {
-        console.log(speed);
-        
         this.speed = {
             x: this.speed.x < 0 ? -speed : speed,
             y: this.speed.y < 0 ? -speed : speed
-        }
-        console.log(this.speed);
-        
+        }        
     }
 }
