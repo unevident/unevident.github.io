1253b84bc153c64bdf3f2804a76767cbbeeb3cba
unevident <linuslee499@gmail.com>
2024-12-28 11:36:32 +0800

    feat: updated site to include links to sorting algo vis

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom"
 export function Navbar() {
     return (
         <div className="flex-1 fixed bg-zinc-950 border-gray-700 text-white ">
-            <Link to='/'><button className="m-2 border-gray-700">Home</button></Link>
-            <Link to='/Brickbreaker'><button className="m-2 border-gray-700">Brick Breaker</button></Link>
+            <Link to='/'><button className="m-2 border-gray-700 border">Home</button></Link>
+            <Link to='/Brickbreaker'><button className="m-2 border-gray-700 border">Brick Breaker</button></Link>
+            <Link to='https://vite-react-three-gilt-15.vercel.app'><button className="m-2 border-gray-700 border">Sorting Algorithm Visualizer</button></Link>
         </div>
     )
 }
