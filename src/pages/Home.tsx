036ff045bd1f5a157d9e154697a9d932e9431991
Unevident <linuslee499@gmail.com>
2025-01-23 16:28:10 +0800

    fix: remove unused button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react"
+// import { useState } from "react"
 import projectsjson from "../projects/projects.json"
 // import { listPosts } from "../Components/list_posts"
 // import { project } from "../Components/project"
 
 export function Home() {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
   
     return (
       <div>
